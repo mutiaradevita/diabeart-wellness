3f707b219b7b6d4632be19245ecc0872fafa947f
elfeny7 <ikmalfaris50@gmail.com>
2023-05-19 07:47:56 +0700

    feat: add admin  authorization on login

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = Auth()->user()->usertype;
+
+            if ($usertype == 'admin') {
+                return redirect()->route('dashboard');
+ 
+        } } else {
+            return view('home');
+        }
+    }
+}
