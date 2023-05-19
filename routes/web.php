f9f52cc379be3136e459242166a4fcc9e9a1f7cc
elfeny7 <ikmalfaris50@gmail.com>
2023-05-19 11:06:37 +0700

    fix: fix ulasan and about route

--- routes/web.php
+++ routes/web.php
@@ -31,12 +31,8 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/home',[HomeController::class, 'index'])->name('home');
 
-Route::get('/about', function(){
-    return view('about');
-});
+Route::get('/about',[HomeController::class, 'about']);
 
-Route::get('/ulasan', function () {
-    return view('ulasan');
-});
+Route::get('/ulasan',[HomeController::class, 'ulasan']);
 
 require __DIR__.'/auth.php';
