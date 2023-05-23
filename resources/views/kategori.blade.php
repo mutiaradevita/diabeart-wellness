7c18d6c23877067001d8572599d7e7480a395848
elfeny7 <ikmalfaris50@gmail.com>
2023-05-23 15:01:54 +0700

    feat: add view category and category card

---
+++ resources/views/kategori.blade.php
@@ -0,0 +1,29 @@
+<x-home-layout>
+    <div class="grid w-full bg-kuning h-[200vh]">
+        <h1 class="absolute font-bebas text-white top-[100px] text-[70px] justify-self-center tracking-[.20em]">Kategori makanan</h1>
+        <h3 class="absolute font-bebas text-oranyet top-[200px] text-[40px] justify-self-center tracking-[.20em]">Food category</h3>
+        <div class="relative flex flex-wrap justify-between top-[290px] px-16">
+            <a href="#">
+                <div class="grid bg-gray-100 w-[200px] h-[300px] hover:bg-gray-200 shadow-lg rounded-3xl">
+                    <div class="absolute justify-self-center bg-white rounded-full w-[130px] h-[130px] top-[30px]"></div>
+                </div>
+            </a>
+            <a href="#">
+                <div class="grid bg-gray-100 w-[200px] h-[300px] hover:bg-gray-200 shadow-lg rounded-3xl">
+                    <div class="absolute justify-self-center bg-white rounded-full w-[130px] h-[130px] top-[30px]"></div>
+                </div>
+            </a>
+            <a href="#">
+                <div class="grid bg-gray-100 w-[200px] h-[300px] hover:bg-gray-200 shadow-lg rounded-3xl">
+                    <div class="absolute justify-self-center bg-white rounded-full w-[130px] h-[130px] top-[30px]"></div>
+                </div>
+            </a>
+            <a href="#">
+                <div class="grid bg-gray-100 w-[200px] h-[300px] hover:bg-gray-200 shadow-lg rounded-3xl">
+                    <div class="absolute justify-self-center bg-white rounded-full w-[130px] h-[130px] top-[30px]"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+</x-home-layout>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,7 +18,7 @@
                     <a href="#" class="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline underline-offset-4">Product</a>
                 </li>
                 <li>
-                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline underline-offset-4">Kategori</a>
+                    <a href="{{route('kategori')}}" class="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline underline-offset-4">Kategori</a>
                 </li>
                 <li>
                     <a href="{{route('ulasan')}}" class="block py-2 pl-3 pr-4 text-gray-500 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline underline-offset-4">Ulasan</a>
