becec223746e28ecb052c3f3a678cbcccd16e79e
elfeny7 <ikmalfaris50@gmail.com>
2023-05-28 19:47:54 +0700

    feat: add some model and migration

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transaksi';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'tanggal',
+        'metode_pembayaran',
+        'id_users',
+        'id_produk',
+        'id_keranjang',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(Users::class);
+    }
+
+    public function keranjang()
+    {
+        return $this->belongsToMany(Keranjang::class, 'keranjang_transaksi');
+    }
+
+
+
+
+}
