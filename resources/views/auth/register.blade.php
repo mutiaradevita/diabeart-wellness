fefd52f0af3f59c71d697f9626c4072610427236
islaarema31 <islaarema31@gmail.com>
2023-06-02 01:04:34 +0700

    fix: update layout register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
-    <div class="grid h-[115vh] bg-kuning w-screen">
+    <div class="grid h-[150vh] bg-kuning w-screen">
         <img src="{{asset('img/logo.png')}}" alt="Diabeart logo" class="absolute w-[18rem] pt-6 justify-self-center">
         <h1 class="absolute top-[260px] md:top-[240px] lg:top-[230px] xl:top-[265px] text-[45px] lg:text-[55px] font-medium justify-self-center font-bebas text-oranye tracking-wide">HI, WELCOME</h1>
-        <div class="absolute place-self-end justify-self-center px-6 pt-7 md:px-7 md:pt-7 md: lg:px-10 lg:pb-[165px] lg:pt-10 bg-hijauab overflow-hidden rounded-t-[40px] w-[20rem] h-[34rem] md:w-[33rem] md:h-[36rem] lg:w-[37rem] lg:h-[38rem] xl:h-[40rem]" style="background-color: rgba(0, 128, 0, 0.3);">
+        <div class="absolute place-self-end justify-self-center px-6 pt-5 md:px-7 md:pt-5 md: lg:px-10 lg:pb-[165px] lg:pt-8 bg-hijauab overflow-hidden rounded-t-[40px] w-[20rem] h-[28rem] md:w-[30rem] md:h-[30rem] lg:w-[31rem] lg:h-[32rem] xl:h-[34rem]" style="background-color: rgba(0, 128, 0, 0.3);">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
 
@@ -59,7 +59,7 @@
             </form>
         </div>
     </div>
-    <div class="grid h-[115vh] bg-oranye place-items-center text-center w-screen">
+    <div class="grid h-[150vh] bg-oranye place-items-center text-center w-screen">
         <div class="lg:tracking-[.30em] xl:tracking-[.45em]">
             <h1 class="text-[55px] text-kuning font-bebas lg:mx-[60px] xl:mx-[100px]">ALREADY HAVE ACCOUNT ?</h1>
             <p class="text-white text-[30px] mx-[30px] lg:mx-[130px] xl:mx-[140px] leading-loose mt-8 font-bebas">Improve your health, shop your healthy food now! enjoy!</p>
