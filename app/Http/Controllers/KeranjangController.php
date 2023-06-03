5a506e77c90f0282f61f6ff2e9e4484027e32443
= <=>
2023-06-03 11:15:55 +0700

    fix: update keranjang & database

--- app/Http/Controllers/KeranjangController.php
+++ app/Http/Controllers/KeranjangController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Keranjang;
 use Illuminate\Http\Request;
 
 class KeranjangController extends Controller
 {
     public function index(){
+        // $Keranjang = Keranjang::where('id_users', $user)->first();
         return view('keranjang');
     }
 }
