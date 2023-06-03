7fcfcda7fe7027a99128f727dc78d1c8a567d180
elfeny7 <ikmalfaris50@gmail.com>
2023-06-03 11:12:29 +0700

    fix: Produk and Detail view

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,6 @@
+<x-home-layout>
+    <div class="grid w-full bg-kuning h-[200vh]">
+        <h1 class="absolute font-bebas text-white top-[100px] text-[70px] justify-self-center tracking-[.20em]">{{$detail->harga ?? 'None'}}</h1>
+    </div>
+
+</x-home-layout>
