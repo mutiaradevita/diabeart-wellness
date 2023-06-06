5505263b881f2286d62360ce77094af1671f1333
elfeny7 <ikmalfaris50@gmail.com>
2023-06-06 23:35:52 +0700

    fix: add to cart and quantity on produk

--- app/Http/Controllers/DetailProdukController.php
+++ app/Http/Controllers/DetailProdukController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Keranjang;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,18 @@ class DetailProdukController extends Controller
 {
     public function index($nama){
         $detail = Produk::where('nama', $nama)->first();
-        return view('detail', compact('detail'));
+        $jumlah = 1;
+        return view('detail', compact('detail', 'jumlah'));
+    }
+
+    public function create(Request $request, $harga, $idproduk, $iduser){
+        $jumlah = $request->get('jumlah');
+        $keranjang = new Keranjang;
+        $keranjang->jumlah = $jumlah;
+        $keranjang->total_harga = $harga * $jumlah;
+        $keranjang->id_produk = $idproduk;
+        $keranjang->id_users = $iduser;
+        $keranjang->save();
+        return redirect()->route('home');
     }
 }
