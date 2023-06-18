858b3b29b51dcfec8bee4960bbdbc44e710d2bc3
= <mutiaradevitaa@gmail.com>
2023-06-18 21:16:56 +0700

    fix:update admin kategori

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -58,11 +58,11 @@
             </th>
             <td class="px-6 py-4">
                 <form action="{{ route('kategori.destroy',$Kategori->id) }}" method="POST">
-                    <a class="font-medium text-blue-600 dark:text-red-500 hover:underline" href="{{ route('kategori.edit', $Kategori->id) }}">Edit</a>
-                    <a class="font-medium text-green-600 dark:text-red-500 hover:underline" href="{{ route('kategori.show', $Kategori->id) }}">Show</a>
+                    <a class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900 justify-content-end" href="{{ route('kategori.edit', $Kategori->id) }}">Edit</a>
+                    <a class="focus:outline-none text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-green-900 justify-content-end" href="{{ route('kategori.show', $Kategori->id) }}">Show</a>
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
+                    <button type="submit" class="focus:outline-none text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-red-900 justify-content-end">Delete</button>
                 </form>
             </td>
         </tr>
