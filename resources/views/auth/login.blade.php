677a4c9564d7c3dba632cbdcb55db3eaa3077a57
elfeny7 <ikmalfaris50@gmail.com>
2023-06-23 19:34:27 +0700

    fix: remove forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,13 +31,6 @@
                         <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-kuning shadow-sm focus:ring-oranye" name="remember">
                         <span class="ml-2 text-[13px] md:text-[14px] lg:text-[16px] text-gray-600">{{ __('Remember me') }}</span>
                     </div>
-                    <div>
-                        @if (Route::has('password.request'))
-                        <a class="underline text-[12px] md:text-[13px] lg:text-[15px] text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                        @endif
-                    </div>
                 </div>
 
                 <div class="flex flex-col place-items-center mt-6">
